188686b47e41e7f3c372e05707b7622c1c893b0e
jmp0x7c00 <990776253@qq.com>
2025-09-22 14:41:33 +0800

    Update IL1MailQueue.sol

--- rollup_contracts/contracts/L1/interfaces/IL1MailQueue.sol
+++ rollup_contracts/contracts/L1/interfaces/IL1MailQueue.sol
@@ -19,4 +19,6 @@ interface IL1MailQueue {
     /// @notice Emitted when a batch is verified.
     /// @param _l1MsgIndex The index of the batch.
     function popMsgs(uint256 _l1MsgIndex) external;
+
+    function queueSize() external view returns(uint256);
 }

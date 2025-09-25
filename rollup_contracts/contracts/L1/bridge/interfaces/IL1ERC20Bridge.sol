4cd662ec50f5413747ad1df828c0ee77c688d7ee
shaoding.zw <shaoding.zw@antgroup.com>
2025-09-25 21:04:27 +0800

    fix rollup N08, Unused imports

--- rollup_contracts/contracts/L1/bridge/interfaces/IL1ERC20Bridge.sol
+++ rollup_contracts/contracts/L1/bridge/interfaces/IL1ERC20Bridge.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
 
-import "./IL1BridgeProof.sol";
-import "../../../common/interfaces/ITokenBridge.sol";
-
 interface IL1ERC20Bridge {
     /**
      *

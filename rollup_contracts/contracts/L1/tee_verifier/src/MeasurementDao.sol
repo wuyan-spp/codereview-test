9c22490478ce0acb95f52b9d44ef9f0e9d76c974
gogogo66 <1639841318@qq.com>
2025-09-25 21:28:45 +0800

    fix: TEE verifier contract  L-03 more

--- rollup_contracts/contracts/L1/tee_verifier/src/MeasurementDao.sol
+++ rollup_contracts/contracts/L1/tee_verifier/src/MeasurementDao.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity 0.8.27;
 
 import {Ownable} from "solady/auth/Ownable.sol";
 import "dcap-attestation/types/Constants.sol";

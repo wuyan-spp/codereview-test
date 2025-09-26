bf4b9b3d7e1bd501799c15aecb250acaf08e473c
shaoding.zw <shaoding.zw@antgroup.com>
2025-09-26 16:54:10 +0800

    fix sequencer L-01 Incomplete Test Suite

---
+++ sequencer_contracts/sys_contract/test/ChainCfg.t.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "forge-std/Test.sol";
+import "../artifact_src/solidity/sys_chaincfg.sol";
+
+contract ChainCfgTest is Test {
+    ChainCfg public chainCfg;
+    address public constant SYS_STAKING = 0x4100000000000000000000000000000000000000;
+    address public constant INTRINSIC_SYS = 0x1111111111111111111111111111111111111111;
+    function setUp() public {
+        chainCfg = new ChainCfg();
+    }
+    function test_initialState() public view {
+        assertEq(chainCfg.rootSys(), address(0));
+    }
+    function test_setConfigAsSysStaking() public {
+        string[] memory keys = new string[](1);
+        string[] memory values = new string[](1);
+        keys[0] = "test.key";
+        values[0] = "test.value";
+        vm.prank(SYS_STAKING);
+        chainCfg.set_config(keys, values);
+    }
+    function test_setConfigAsIntrinsicSys() public {
+        string[] memory keys = new string[](1);
+        string[] memory values = new string[](1);
+        keys[0] = "test.key";
+        values[0] = "test.value";
+        vm.prank(INTRINSIC_SYS);
+        chainCfg.set_config(keys, values);
+    }
+    function test_setConfigUnauthorized() public {
+        string[] memory keys = new string[](1);
+        string[] memory values = new string[](1);
+        keys[0] = "test.key";
+        values[0] = "test.value";
+        vm.prank(address(0x1234));
+        vm.expectRevert("Not owner");
+        chainCfg.set_config(keys, values);
+    }
+    function test_setConfigKeysValuesMismatch() public {
+        string[] memory keys = new string[](1);
+        string[] memory values = new string[](2);
+        keys[0] = "key";
+        values[0] = "value1";
+        values[1] = "value2";
+        vm.prank(INTRINSIC_SYS);
+        vm.expectRevert("KVs are not match");
+        chainCfg.set_config(keys, values);
+    }
+    function test_getNonExistentConfig() public view {
+        assertEq(chainCfg.get_config("nonexistent"), "");
+    }
+    function test_changeSysOwner() public {
+        address newOwner = address(0x1234);
+        vm.prank(INTRINSIC_SYS);
+        chainCfg.changeSys(newOwner);
+        assertEq(chainCfg.rootSys(), newOwner);
+    }
+}
---
+++ sequencer_contracts/sys_contract/test/PermissionControl.t.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "forge-std/Test.sol";
+import "../artifact_src/solidity/permission_control.sol";
+contract PermissionControlTest is Test {
+    PermissionControl public permissionControl;
+    address public owner;
+    address public grantee1;
+    address public grantee2;
+    function setUp() public {
+        permissionControl = new PermissionControl();
+        owner = address(this);
+        grantee1 = address(0x1234);
+        grantee2 = address(0x5678);
+    }
+    function test_initialState() public view {
+        assertEq(permissionControl.getSuperAdmin(), owner);
+        assertEq(permissionControl.getGranteeAdmin().length, 0);
+    }
+    function test_grantAdminAsOwner() public {
+        permissionControl.grantAdmin(grantee1);
+        
+        address[] memory grantees = permissionControl.getGranteeAdmin();
+        assertEq(grantees.length, 1);
+        assertEq(grantees[0], grantee1);
+    }
+    function test_grantAdminAsNonOwner() public {
+        vm.prank(grantee1);
+        vm.expectRevert("Permission denied");
+        permissionControl.grantAdmin(grantee2);
+    }
+    function test_grantAdminDuplicate() public {
+        permissionControl.grantAdmin(grantee1);
+        
+        vm.expectRevert("Address already exist in grantees");
+        permissionControl.grantAdmin(grantee1);
+    }
+    function test_revokeAdminAsOwner() public {
+        permissionControl.grantAdmin(grantee1);
+        permissionControl.revokeAdmin(grantee1);
+        
+        address[] memory grantees = permissionControl.getGranteeAdmin();
+        assertEq(grantees.length, 0);
+    }
+    function test_revokeAdminAsNonOwner() public {
+        permissionControl.grantAdmin(grantee1);
+        
+        vm.prank(grantee1);
+        vm.expectRevert("Permission denied");
+        permissionControl.revokeAdmin(grantee1);
+    }
+    function test_revokeNonExistentAdmin() public {
+        vm.expectRevert("Address not exist in grantees");
+        permissionControl.revokeAdmin(grantee1);
+    }
+    function test_transferSuperAdminAsOwner() public {
+        address newOwner = address(0x9999);
+        permissionControl.tranferSuperAdmin(newOwner);
+        assertEq(permissionControl.getSuperAdmin(), newOwner);
+    }
+    function test_transferSuperAdminAsNonOwner() public {
+        vm.prank(grantee1);
+        vm.expectRevert("Permission denied");
+        permissionControl.tranferSuperAdmin(grantee2);
+    }
+    function test_transferSuperAdminToSameAddress() public {
+        vm.expectRevert("Permission denied, same address");
+        permissionControl.tranferSuperAdmin(owner);
+    }
+    function test_adminManagementFlow() public {
+        permissionControl.grantAdmin(grantee1);
+        permissionControl.grantAdmin(address(0x5678));
+        
+        address[] memory grantees = permissionControl.getGranteeAdmin();
+        assertEq(grantees.length, 2);
+        
+        permissionControl.revokeAdmin(grantee1);
+        
+        grantees = permissionControl.getGranteeAdmin();
+        assertEq(grantees.length, 1);
+        assertEq(grantees[0], address(0x5678));
+    }
+}

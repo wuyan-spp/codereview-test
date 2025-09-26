5da287abed05c6fc7e19b4f19d01b852b157bf65
shaoding.zw <shaoding.zw@antgroup.com>
2025-09-26 18:27:16 +0800

    fix sequencer M02 more

--- sequencer_contracts/sys_contract/artifact_src/solidity/sys_chaincfg.sol
+++ sequencer_contracts/sys_contract/artifact_src/solidity/sys_chaincfg.sol
@@ -172,7 +172,7 @@ contract ChainCfg {
         // Have both effective and pending configs
         ConfigCheckpoint storage latestCp = configCps[1];
         // Build merged configuration using helper function
-        Config[] memory baseConfigs = (block.number >= latestCp.effectiveBlockNum) ? configCps[0].configs : latestCp.configs;
+        Config[] memory baseConfigs = latestCp.configs;
         Config[] memory mergedConfigs = _buildMergedConfig(baseConfigs, keys, values);
         
         if (block.number >= latestCp.effectiveBlockNum) {
